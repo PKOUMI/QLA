586834cd0373b99f24a0d62c08382a28cdead5b2
PKOUMI <ploutiskoumi@gmail.com>
2026-08-22 23:15:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ window.QLA_CONFIG = {
    *   'https://qla-api.vercel.app'
    *   'https://api.yourdomain.co.uk'
    */
-  apiBaseUrl: '',
+  apiBaseUrl: 'https://qla-virid.vercel.app',
 
   /** Shown in the email header. Optional. */
   schoolName: '',
